de3d827a41b9802b8d29c5fc0d8dba6c83491eb1
Ayan banerjee <ayanb1999@gmail.com>
2018-12-07 20:13:36 +0530

    added vuetify

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Dashboard</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer v-model="drawer" app class="indigo">
+            <p>Test</p>
+        </v-navigation-drawer>
+
+    </nav>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            drawer: false
+        }
+    }
+}
+</script>
